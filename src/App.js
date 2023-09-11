c385e681e50f53003503c8127bc42e1ad7c7477e
Hassan <malikhassanhu55@gmail.com>
2023-09-11 11:26:15 +0500

    Start Project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="">
+   <h1 className='text-3xl text-red-600 font-bold'>Hassan</h1>
+    </div>
+  );
+}
+
+export default App;
