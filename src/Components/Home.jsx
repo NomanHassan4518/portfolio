5c6a987f8c56ec66bd6ba0c6a3d2633b84d251e9
Noman Hassan <137282216+NomanHassan4518@users.noreply.github.com>
2024-05-14 21:18:14 +0500

    Update Home.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,46 +1,34 @@
-import React from 'react';
-import 'slick-carousel/slick/slick.css';
-import photo1 from '../photo1.png'
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaLinkedinIn } from "react-icons/fa6";
+import { FaGithub } from "react-icons/fa"
 
 const Home = () => {
-
-
-
   return (
-    <section id='home' className=' relative '>
-      <div>
-        <div className={`xl:px-12 px-4 w-full lg:h-[45rem] xl:h-[50rem] !grid grid-cols-12   `}  >
-          <div className='lg:col-span-6 col-span-12' >
-            <div className='flex justify-start mt-12  md:mt-10 lg:mt-28 items-center md:justify-start md:items-center lg:items-start  px-5 flex-col lg:space-y-4 slide-content '>
-              <p className='text-[#ffbd39]  font-semibold text-lg'>Hello!</p>
-              <div className='flex lg:flex-col md:flex-row flex-col items-center md:items-start '>
-                <div className='flex space-x-2 font-[900]  lg:text-[70px] text-5xl '>
-                  <h1 className='   '>I'm a</h1>
-                  <h1 className='text-[#ffbd39]  '>React</h1>
-                </div>
-                <div className="flex space-x-2 font-[900] mt-4  lg:text-[70px] text-5xl ">
-                  <h1 className="text-white  ">Developer </h1>
-                  <h1 className="text-[#ffbd39]"> based</h1>
-                </div>
-              </div>
-              <div className='flex space-x-2 font-[900]  lg:text-[50px] xl:text-[70px] text-4xl  '>
-                <h1 className="text-[#ffbd39] mt-3">in Islamabad</h1>
-              </div>
-            </div>
-          </div>
+    <section id="home" className=" relative -top-20">
+      <div
+        className="w-full md:h-[42rem] h-[38rem] bg-black flex items-center justify-center flex-col"
+        style={{
+          backgroundImage: `url("https://davis-html.netlify.app/assets/img/hero-bg-2.jpg")`,
+          backgroundPosition: "center center",
+          backgroundSize: "cover",
+        }}
+      >
+        <h1 className="md:text-[60px] text-[30px] font-[900] space-x-5 opacity-100">
+          <span>Hi, I'm</span>
+          <span className="text-[#fec544]">Noman Hassan</span>
+        </h1>
+        <p className="md:mt-9 mt-5 lg:w-[65%] md:w-[90%] w-full px-3 md:px-0 md:text-center text-xl leading-9 text-gray-300 ">
+        Frontend developer specializing in React.js, passionate about crafting dynamic and user-friendly interfaces. Dedicated to optimizing performance and enhancing user experiences through innovative solutions.
+        </p>
 
-          <div className=" col-span-12  lg:col-span-6   w-full lg:w-full h-full     ">
-            <div >
-              <img src={photo1} alt="" className='w-full  h-full lg:h-[39rem]  object-cover  ' />
-            </div>
-          </div>
+        <div className="mt-12 flex items-center space-x-10 ">
+          <Link to="https://www.linkedin.com/in/noman-hassan-36a983270/" target='_blank' className="border-2 border-gray-600 hover:border-white rounded-full text-center w-[50px] h-[50px] flex items-center justify-center hover:text-yellow-300 "><FaLinkedinIn className="w-[20px] h-[20px]"/></Link>
+          <Link to="https://github.com/NomanHassan4518" target="_blank" className="border-2 border-gray-600 hover:border-white rounded-full text-center w-[50px] h-[50px] flex items-center justify-center hover:text-yellow-300 "><FaGithub className="w-[20px] h-[20px]"/></Link>
         </div>
-
-
       </div>
-
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
